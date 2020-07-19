84ba3da859118450b2bc938b42d0103b78b54bdb
Matthew Rindel <matthew@sobantu.co.za>
2020-07-19 17:27:49 +0200

    Ignored more files
    Added build folder to conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ class LangOcrConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = "qt/5.14.2@bincrafters/stable", "tesseract/4.1.1", "leptonica/1.79.0", "zstd/1.4.5"
     generators = "cmake", "cmake_find_package"
-
+    build_folder = "build"
 
     def build(self):
         cmake = CMake(self)
