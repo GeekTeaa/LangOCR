7e227defac1cf6cf5d2d8190284cfe38a14792ec
Matthew <matthew@sobantu.co.za>
2020-07-25 14:20:51 +0200

    Added cmake as a build requirement

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, CMake, tools
 
 class LangOcrConan(ConanFile):
+    build_requires = "cmake/3.17.3"
     settings = "os", "compiler", "build_type", "arch"
     requires = "qt/5.14.2@bincrafters/stable", "tesseract/4.1.1", "leptonica/1.79.0", "zstd/1.4.5"
     generators = "cmake", "cmake_find_package"
